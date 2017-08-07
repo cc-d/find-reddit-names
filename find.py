d54b96b84c807a9b41f9321106098cdbe8f08c4a
cc-d <ccarterdev@gmail.com>
2017-08-06 20:14:09 -0400

    look for names without - and _

--- find.py
+++ find.py
@@ -9,27 +9,26 @@ def main():
     delay = 0.1 # time between requests
     user_agent = 'Just looking for some unique usernames 1.0'
 
-    chars = 'abcdefghijklmnopqrstuvwxyz123456789-_'
+    chars = 'abcdefghijklmnopqrstuvwxyz123456789'# already looked for names with - _
     names = []
 
     for x in product(chars, repeat=3):
         j = ''.join(x)
-        #if j[0] != '-' and j[len(j) -1] != '-':
-        if j.find('-') != -1 or j.find('_') != -1: # look for only  names with - or _
-            names.append(''.join(x))
 
     shuffle(names)
     mnames = len(names)
     count = 0
     print mnames, 'possible names.'
-    print 'It will take', ((mnames / delay)/60)/60, 'hours to finish at minimum. Sit back, grab some coffee, screen and forget.'
+    print 'It will take', ((mnames * delay)/60)/60, 'hours to finish at minimum. Sit back, grab some coffee, screen and forget.'
 
     for name in names:
         headers = {'User-Agent': user_agent}
         r = requests.get('https://www.reddit.com/user/%s' % name, headers=headers)
         if r.status_code == 404:
             sleep(2)
-            if check(name):
+            result = check(name)
+            print result
+            if result:
                 with open('possible.txt', 'a+') as p:
                     p.write(name + '\n')
                 print 'Possible Name!:', name
